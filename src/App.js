ec6f8aaf1dbadb32060572cdf9a9dadc5b2a2561
asimov <seyranmustafa4@gmail.com>
2024-09-06 07:14:47 +0300

    random user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import mailSvg from "./assets/mail.svg";
 import manSvg from "./assets/man.svg";
 import womanSvg from "./assets/woman.svg";
@@ -15,63 +15,74 @@ const url = "https://randomuser.me/api/";
 const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
 function App() {
+  const [people, setPeople] = useState([]);
+  // console.log(people.name);
+  const getBilgiler = async () => {
+    const res = await axios.get(url);
+    console.log(res.data.results);
+    setPeople(res.data.results);
+  };
+  //
 
-  const data= async()=>{
-axios()
-  }
+  useEffect(() => {
+    getBilgiler();
+  }, []);
+
   return (
     <main>
       <div className="block bcg-orange">
         <img src={cwSvg} alt="cw" id="cw" />
       </div>
       <div className="block">
-        <div className="container">
-          <img src={defaultImage} alt="random user" className="user-img" />
-          <p className="user-title">My ... is</p>
-          <p className="user-value"></p>
-          <div className="values-list">
-            <button className="icon" data-label="name">
-              <img src={womanSvg} alt="user" id="iconImg" />
-            </button>
-            <button className="icon" data-label="email">
-              <img src={mailSvg} alt="mail" id="iconImg" />
-            </button>
-            <button className="icon" data-label="age">
-              <img src={womanAgeSvg} alt="age" id="iconImg" />
-            </button>
-            <button className="icon" data-label="street">
-              <img src={mapSvg} alt="map" id="iconImg" />
-            </button>
-            <button className="icon" data-label="phone">
-              <img src={phoneSvg} alt="phone" id="iconImg" />
-            </button>
-            <button className="icon" data-label="password">
-              <img src={padlockSvg} alt="lock" id="iconImg" />
-            </button>
-          </div>
-          <div className="btn-group">
-            <button className="btn" type="button">
-              new user
-            </button>
-            <button className="btn" type="button">
-              add user
-            </button>
-          </div>
+        {people.map(({cell,email,location,name,phone,registered,picture,login,gender}) => (
+          <div key={cell} className="container">
+            <img src={picture.large} alt="random user" className="user-img" />
+            <p className="user-title">My name is</p>
+            <p className="user-value">{name.title} {name.first} {name.last}</p>
+            <div className="values-list">
+              <button className="icon" data-label="name">
+                <img src={gender==="female" ? womanSvg:manSvg} alt="user" id="iconImg" />
+              </button>
+              <button className="icon" data-label="email">
+                <img src={mailSvg} alt="mail" id="iconImg" />
+              </button>
+              <button className="icon" data-label="age">
+                <img src={womanAgeSvg} alt="age" id="iconImg" />
+              </button>
+              <button className="icon" data-label="street">
+                <img src={mapSvg} alt="map" id="iconImg" />
+              </button>
+              <button className="icon" data-label="phone">
+                <img src={phoneSvg} alt="phone" id="iconImg" />
+              </button>
+              <button className="icon" data-label="password">
+                <img src={padlockSvg} alt="lock" id="iconImg" />
+              </button>
+            </div>
+            <div className="btn-group">
+              <button className="btn" type="button">
+                new user
+              </button>
+              <button className="btn" type="button">
+                add user
+              </button>
+            </div>
 
-          <table className="table">
-            <thead>
-              <tr className="head-tr">
-                <th className="th">Firstname</th>
-                <th className="th">Email</th>
-                <th className="th">Phone</th>
-                <th className="th">Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="body-tr"></tr>
-            </tbody>
-          </table>
-        </div>
+            <table className="table">
+              <thead>
+                <tr className="head-tr">
+                  <th className="th">Firstname</th>
+                  <th className="th">Email</th>
+                  <th className="th">Phone</th>
+                  <th className="th">Age</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="body-tr"></tr>
+              </tbody>
+            </table>
+          </div>
+        ))}
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <Footer />
